f829520f0d0255d15b034cb44316eb3934067200
Alien501 <cvignesh404@gmail.com>
2024-06-17 10:29:14 +0530

    lets see

--- backend/api/index.js
+++ backend/api/index.js
@@ -13,9 +13,9 @@ const PORT = process.env.PORT || 3000;
 // Middlewares
 app.use(express.json());
 app.use(cors(
-    {
-        origin: process.env.CURRENTDEPLOY
-    }
+    // {
+    //     origin: process.env.CURRENTDEPLOY
+    // }
 ));
 app.use(logger);
 
