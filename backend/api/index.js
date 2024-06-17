c563b71bff1427434aeefd7eaa26bd15e9474c82
Alien501 <cvignesh404@gmail.com>
2024-06-17 10:24:31 +0530

    dono

--- backend/api/index.js
+++ backend/api/index.js
@@ -12,7 +12,11 @@ const PORT = process.env.PORT || 3000;
 
 // Middlewares
 app.use(express.json());
-app.use(cors());
+app.use(cors(
+    {
+        origin: process.env.CURRENTDEPLOY
+    }
+));
 app.use(logger);
 
 app.use('/api/v1', router);
